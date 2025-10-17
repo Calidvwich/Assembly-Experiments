5cb60d5780ce28611dae7168879341505e3f03d3
calidvwich <arthurxjz@gmail.com>
2025-10-17 08:57:04 +0800

    20251017_01

--- exp1/lexer.cpp
+++ exp1/lexer.cpp
@@ -5,68 +5,70 @@
 #include <unordered_set>
 
 using namespace std;
-
+// 定义Token结构体。一个token应该包含序号index，类型type以及具体的内容value
 struct Token {
     int index;
     string type;
     string value;
 };
-
 // 关键字集合
 unordered_set<string> keywords = {
     "int", "void", "if", "else", "while", "break", "continue", "return"
 };
-
 // 双字符运算符
 unordered_set<string> doubleOps = {"==", "!=", "<=", ">=", "&&", "||"};
-
 // 单字符运算符
 unordered_set<char> singleOps = {
     '+','-','*','/','%','<','>','=','!','(',')','{','}',';',','
 };
-
+// 判断标识符首字母，按照题目要求，只能是字母or_
 bool isIdentStart(char c) {
     return isalpha(c) || c == '_';
 }
-
+// 判断标识符的其他部分，按照题目要求只能是字母or数字or_
 bool isIdentPart(char c) {
     return isalnum(c) || c == '_';
 }
-
+// 判断是不是数字
 bool isDigit(char c) {
     return isdigit(c);
 }
-
 // 跳过空白和注释
 void skipWhitespaceAndComments(string &code, size_t &pos) {
     while (pos < code.size()) {
+        // 跳过空格
         if (isspace(code[pos])) {
             ++pos;
-        } else if (code[pos] == '/' && pos + 1 < code.size()) {
+        }
+        // 跳过注释。无论是多行注释还是单行注释一定是/开头，因此读取到/时开始检测。 
+        else if (code[pos] == '/' && pos + 1 < code.size()) {
             if (code[pos + 1] == '/') {
-                // 单行注释
+                // 跳过单行注释，检测到//时，该行是单行注释，遍历到换行（本行结束）即可
                 pos += 2;
                 while (pos < code.size() && code[pos] != '\n') ++pos;
             } else if (code[pos + 1] == '*') {
-                // 多行注释
+                // 跳过多行注释，检测到/*时，一定是多行注释。因此只需要不断循环直到*/连续出现，此时多行注释结束
                 pos += 2;
                 while (pos + 1 < code.size() && !(code[pos] == '*' && code[pos + 1] == '/')) ++pos;
                 if (pos + 1 < code.size()) pos += 2;
             } else {
-                break;
+                break;// 不是上述情况，忽略。
             }
         } else {
             break;
         }
     }
 }
-
+// 词法分析主函数。思路如下：
+// 遍历整个代码的时候，重复调用上面的函数跳过注释和空格。
+// 确认不是上述可忽略情况后，则对象一定是一个token，判断类型即可
 vector<Token> tokenize(const string &code) {
     vector<Token> tokens;
     size_t pos = 0;
     int index = 0;
 
     while (pos < code.size()) {
+        // 先决判断是否可忽略。
         skipWhitespaceAndComments(const_cast<string&>(code), pos);
         if (pos >= code.size()) break;
 
