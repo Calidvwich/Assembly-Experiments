37b874827d6b0a63c6aaf63afe360815dc7bc928
calidvwich <arthurxjz@gmail.com>
2025-11-09 17:06:11 +0800

    get code!

--- exp2/toycc.cpp
+++ exp2/toycc.cpp
@@ -1,10 +1,25 @@
-// generate_test.cpp - Outputs test C code
+// print_file.cpp - Outputs the contents of a file
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main(){
-    cout << "int main(){\n";
-    cout << "    return 32;\n";
-    cout << "}\n";
+int main(int argc, char* argv[]){
+    if(argc < 2) {
+        cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+    
+    ifstream file(argv[1]);
+    if(!file) {
+        cerr << "Error: Cannot open file " << argv[1] << "\n";
+        return 1;
+    }
+    
+    string line;
+    while(getline(file, line)) {
+        cout << line << "\n";
+    }
+    
     return 0;
 }
